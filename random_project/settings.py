4cc1debfffedc32c9dbb5c68a01491b0a57f391a
thompson-pilot <rthomques@gmail.com>
2023-03-05 11:38:46 -0300

    static files

--- random_project/settings.py
+++ random_project/settings.py
@@ -131,8 +131,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
 STATIC_URL = "/static/"
-STATICFILES_DIRS = [str(BASE_DIR.joinpath("static"))]  # new
-STATIC_ROOT = str(BASE_DIR.joinpath("staticfiles"))  # new
+STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_ROOT = BASE_DIR / "staticfiles"
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
